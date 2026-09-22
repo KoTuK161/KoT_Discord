3977774ffec5fab7792f725dd0f5edd2fabc43a1
KoTuK161 <simaksergey18@gmail.com>
2026-09-22 21:51:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -564,13 +564,13 @@ async def on_ready():
         await bot.change_presence(
             status=discord.Status.online,
             activity=discord.Game(
-                "Играет в шахматы"
+                "Играет на баяне"
             )
         )
 
         log.info(
             "Статус бота установлен: "
-            "'Играет в шахматы'"
+            "'Играет на баяне'"
         )
 
     except Exception as e:
